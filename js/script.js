1775bdbacdf8972016d0c24169cd6165f99599e3
Kevin Davalos <kevin.davalos.01@gmail.com>
2022-09-08 22:41:55 -0300

    Se implementa comentarios en las lineas de codigo

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaProductos = document.querySelector('#lista-productos');
 
+// Crea los productos a HTML
 const creandoProductos = async() => {
     const response = await fetch("../secciones/data.json");
     const productosCreados = await response.json();
@@ -26,6 +27,7 @@ const creandoProductos = async() => {
 creandoProductos();
 let productosCarrito = [];
 
+// Local storage Carrito
 document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('productosCarrito')){
         productosCarrito = JSON.parse(localStorage.getItem('productosCarrito'))
@@ -35,11 +37,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 registrarEventListeners();
 function registrarEventListeners() {
+    // Agrega un producto usando "Agregar al carrito"
     listaProductos.addEventListener('click', agregarProducto);
+    // Elimina productos del carrito
     carrito.addEventListener('click', eliminarProducto);
-    
+    // Vaciar el carrito
     vaciarCarritoBtn.addEventListener('click', () => {
-        productosCarrito = [];
+        productosCarrito = []; // Reseteo del array
         Swal.fire({
             position: 'top-end',
             icon: 'success',
@@ -47,7 +51,7 @@ function registrarEventListeners() {
             showConfirmButton: false,
             timer: 1000
         })
-        limpiarHTML();
+        limpiarHTML(); // Eliminamos todo el HTML
     })
 }
 
@@ -68,8 +72,9 @@ function eliminarProducto(e) {
     }
 }
 
-
+// Lee el contenido del HTML que hagamos click y extrae la info
 function leerDatosProducto(producto) {
+    // Crea un objeto con el contenido del producto actual
     const infoProducto = {
         nombre: producto.querySelector('h3').textContent,
         precio: producto.querySelector('p').textContent,
@@ -77,13 +82,16 @@ function leerDatosProducto(producto) {
         cantidad: 1
     }
 
+    // Verifica si un elemento ya exite en el carrito para actualizar la cantidad
     const existeProducto = productosCarrito.some( producto => producto.id === infoProducto.id);
     if(existeProducto) {
+        // Actualiza la cantidad
         const productoMas = productosCarrito.map( producto => {
             if(producto.id === infoProducto.id){
                 producto.cantidad++;
                 return producto;
             }else {
+                // Agrega el elemento al arreglo del carrito
                 return producto;
             }
         } );
@@ -94,9 +102,12 @@ function leerDatosProducto(producto) {
     carritoHTML();
 }
 
+// Muestra el carrito de compras en el HTML
 function carritoHTML() {
+    //limpia el HTML
     limpiarHTML();
 
+    // Recorre el carrito y genera el HTML
     productosCarrito.forEach(producto => {
         const {nombre, precio, cantidad, id} = producto;
         const row = document.createElement('tr')
@@ -108,11 +119,14 @@ function carritoHTML() {
                             <a href="#" class="carritoEliminar" data-id="${id}"> X </a>
                         </td>
                         `;
+        // Agrega el HTML del carrito en el tbody
         contenedorCarrito.appendChild(row);
+        // Renderizado de localStorage en el carrito
         localStorage.setItem('productosCarrito', JSON.stringify(productosCarrito))
     });
 }
 
+// Elimina los productos del tbody
 function limpiarHTML() {
     while(contenedorCarrito.firstChild) {
         contenedorCarrito.removeChild(contenedorCarrito.firstChild)
